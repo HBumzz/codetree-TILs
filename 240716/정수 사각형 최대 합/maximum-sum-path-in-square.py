bcc5b069624f9dcab64612cf69f03d7b10875108
Choi Hong Jun <110510311+HBumzz@users.noreply.github.com>
2024-07-16 14:56:13 +0900

    Title: 정수 사각형 최대 합, Time: 114ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240716/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+arr = [list(map(int, input().split())) for _ in range(n)]
+dp = [[0]*n for _ in range(n)]
+def initial():
+    dp[0][0] = arr[0][0]
+    for i in range(1,n):
+        dp[0][i] = dp[0][i-1] + arr[0][i]
+    for i in range(1,n):
+        dp[i][0] = dp[i-1][0] + arr[i][0]
+initial()
+
+for i in range(1,n):
+    for j in range(1,n):
+        dp[i][j] = max(dp[i-1][j] + arr[i][j], dp[i][j-1] + arr[i][j])
+print(dp[n-1][n-1])
